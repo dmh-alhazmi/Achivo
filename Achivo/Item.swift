1f71f6a4a0f1df7480e70d9e95162a07bb51afa7
dmh-alhazmi <dmh.alhazmi@gmail.com>
2026-05-03 11:38:03 +0300

    Initial Commit

---
+++ Achivo/Item.swift
@@ -0,0 +1,18 @@
+//
+//  Item.swift
+//  Achivo
+//
+//  Created by Deemah Alhazmi on 03/05/2026.
+//
+
+import Foundation
+import SwiftData
+
+@Model
+final class Item {
+    var timestamp: Date
+    
+    init(timestamp: Date) {
+        self.timestamp = timestamp
+    }
+}
